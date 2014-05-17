b34b2e2fa4fdf9fc2113c88d98ba70681bc1299b
deigote <diego@toharia.com>
2014-05-17 19:26:29 +0200

    Fixed completions to not depend on a fixed path but rather use fish variables to find the actual function implementation file

--- completions/gh-issue.fish
+++ completions/gh-issue.fish
@@ -1,4 +1,7 @@
-source $HOME/.config/fish/functions/gh-issue.fish
+for i in $fish_function_path
+	if test -d $i ; set existing_paths $existing_paths $i ; end
+end
+source (find $existing_paths -name 'gh-issue.fish')
 for option in (functions | grep gh-issue- | awk -F 'gh-issue-' '{print $2}')
         complete -c gh-issue -a $option
 end
