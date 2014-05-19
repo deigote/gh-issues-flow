b087f06680e5a98b26a68bee623fe97b3939e332
deigote <diego@toharia.com>
2014-05-19 19:35:36 +0200

    Is easier to find gh-isshe.fish using find command... as the name says :)

--- completions/gh-issue.fish
+++ completions/gh-issue.fish
@@ -1,7 +1,4 @@
-for i in $fish_function_path
-	if test -d $i ; set existing_paths $existing_paths $i ; end
-end
-source (find $existing_paths -name 'gh-issue.fish')
+source (find $fish_function_path -name 'gh-issue.fish')
 for option in (functions | grep gh-issue- | awk -F 'gh-issue-' '{print $2}')
         complete -c gh-issue -a $option
 end
