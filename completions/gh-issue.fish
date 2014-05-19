6f6a2155d71a5faa2587fd915fd173d00e0963e5
deigote <diego@toharia.com>
2014-05-19 19:48:47 +0200

    Using ls and xargs to only search in existing directories

--- completions/gh-issue.fish
+++ completions/gh-issue.fish
@@ -1,4 +1,4 @@
-source (find $fish_function_path -name 'gh-issue.fish')
+source (find (echo $fish_function_path | xargs ls -d 2>/dev/null) -name 'gh-issue.fish')
 for option in (functions | grep gh-issue- | awk -F 'gh-issue-' '{print $2}')
         complete -c gh-issue -a $option
 end
